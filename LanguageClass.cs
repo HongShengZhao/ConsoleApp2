2d392dd879f23b473cf98a0111f35344a1a719de
Hotsun <412865108@qq.com>
2019-11-21 11:18:03 +0800

    增加语言并完成测试

---
+++ LanguageClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class LanguageClass
+    {
+        public void Init()
+        {
+            Console.WriteLine("增加语言界面包");
+        }
+    }
+}
